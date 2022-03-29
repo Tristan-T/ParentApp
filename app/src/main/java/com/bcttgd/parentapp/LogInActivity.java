2d3ec7ed3c7b7c1f3cc7c1a354907c39c1e8d5c4
Tristan <tristan.tribes@etu.umontpellier.fr>
2022-03-29 15:14:01 +0200

    Initial commit

---
+++ app/src/main/java/com/bcttgd/parentapp/LogInActivity.java
@@ -0,0 +1,14 @@
+package com.bcttgd.parentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
---
+++ app/src/main/java/com/bcttgd/parentapp/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.bcttgd.parentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
