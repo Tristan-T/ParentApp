87dff23d09623c6c7e011b4d25840b76fd60008f
Tristan-Tribes <43269571+Tristan-T@users.noreply.github.com>
2022-04-05 15:15:14 +0200

    Bottom Navigation Menu in MainActivity

--- app/src/main/java/com/bcttgd/parentapp/MainActivity.java
+++ app/src/main/java/com/bcttgd/parentapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.bcttgd.parentapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get FragmentContainer
+        FragmentContainerView fragmentContainerView = findViewById(R.id.fragmentContainerMain);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Google recommends to use if instead of switches}
+                if(item.getItemId() == R.id.bottom_navigation_menu_item_data) {
+
+                    return true;
+                }
+
+                if(item.getItemId() == R.id.bottom_navigation_menu_item_camera) {
+
+                    return true;
+                }
+
+                if(item.getItemId() == R.id.bottom_navigation_menu_item_map) {
+
+                    return true;
+                }
+
+                if(item.getItemId() == R.id.bottom_navigation_menu_item_alert) {
+
+                    return true;
+                }
+
+                if(item.getItemId() == R.id.bottom_navigation_menu_item_account) {
+
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
